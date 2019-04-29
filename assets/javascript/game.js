a685c3c4e57977cf897649aa90bb6d92ae4ef6cb
Kelsi H <kelsimhoyle@gmail.com>
2019-04-29 08:49:16 -0600

    creating basics of the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,58 @@
 // variables for the game
-//number of guesses left
-var guessesLeft = 10;
+var gameControls = {
+    alphabet: 'abcdefghijklmnopqrstuvwxyz'.split(''),
+    guessesLeft: 10,
+    gamesWon: 0,
 
-//incorrect letters guess
-var wrongLetter = [];
-
-//games won
-var gamesWon = 0;
-
-// an array of words to be randomly chosen to guess
-var wordList = ["Beets", "Dwight", "Jim", "Pam", "Sprinkles", "Scranton", "Dunder Mifflin", "That's What She Said", "Goodbye Toby"];
-
+}
+var wordList = ["beets", "dwight", "jim", "pam", "sprinkles", "scranton"];
 // variable for the random word to be guessed
-var secretWord = wordList[Math.floor(Math.random()*wordList.length)];
-
+var secretWord = wordList[Math.floor(Math.random() * wordList.length)];
 // splitting the word into seperate letters
 var secretLetters = secretWord.split('');
 
+// console.log(secretLetters);
+
+// array of user's guesses
+var wrongGuess = [];
+var correctGuess = [];
+
+// variables to edit text on html document
+var guessesLeftText = document.getElementById("guesses-left-text");
+var wrongGuessText = document.getElementById("wrong-guess-text");
+
+var secretLettersText = document.getElementById("secret-word-text");
+
 // create blank spots to be filled and guessed
-document.onkeyup = function(event) {
-var letterGuess = event.key;
-console.log(letterGuess);
+for (var i = 0; i < secretLetters.length; i++) {
+    if (letterGuess !== secretLetters) {
+        secretLettersText.textContent += "_ ";
+    } if (letterGuess === secretLetters) {
+        secretLetters.indexOf(letterGuess)
+    }
 }
+
+// user starts playing the game
+document.onkeyup = function (event) {
+    var letterGuess = event.key.toLowerCase();
+    if (gameControls.alphabet.includes(letterGuess)) {
+        if (gameControls.guessesLeft > 0) {
+            if (secretLetters.includes(letterGuess)) {
+                correctGuess.push(letterGuess)
+                console.log("yay");
+            } else if (wrongGuess.includes(letterGuess)) {
+                alert("You already guessed that!");
+            }
+            else {
+                gameControls.guessesLeft--;
+                wrongGuess.push(letterGuess);
+            }
+
+            guessesLeftText.textContent = gameControls.guessesLeft;
+            wrongGuessText.textContent = wrongGuess.join(' ').toUpperCase();
+        }
+    } else {
+        alert("Press a letter to make a guess!");
+    }
+}
+
